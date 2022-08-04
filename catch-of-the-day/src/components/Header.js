1e413e9ba6849d30d47ea553b2355240b923435f
Kevin Deal <kevin@superfriend.ly>
2022-08-04 13:42:33 -0400

    header components props

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -4,9 +4,16 @@ class Header extends React.Component {
   render() {
     return (
       <header className="top">
-        <h1>Catch<span className="ofThe">of the</span> Day</h1>
+        <h1>
+          Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
         <h3 className="tagline">
-          <span>Fresh Seafood Market</span>
+          <span>{this.props.tagline}</span>
         </h3>
       </header>
     );
