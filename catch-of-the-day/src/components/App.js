63c6d48e3e35fb61b845344ac40fc5ccbbe8b1c2
Kevin Deal <kevin@superfriend.ly>
2022-08-12 14:55:10 -0400

    components refactor

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,18 +3,14 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="catch-of-the-day">
-        <div className="menu">
-          <Header tagline="Fresh  Seafood Market"/>
-        </div>
-        <Order />
-        <Inventory />
+export default function App() {
+  return (
+    <div className="catch-of-the-day">
+      <div className="menu">
+        <Header tagline="Fresh Seafood Market"/>
       </div>
-    )
-  } 
+      <Order />
+      <Inventory />
+    </div>
+  )
 }
-
-export default App; 
